039c4acb2f4a1e76c2c13f6b4222b38d9477aa08
chien <chientthtt@gmail.com>
2025-05-26 22:36:15 +0700

    add

---
+++ Book-Shop.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int price[1005],pages[1005];
+int dp[1005][100005];
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n,x;
+    cin>>n>>x;
+    memset(dp,0,sizeof(dp));
+    for(int i=1;i<=n;i++)
+    {
+        cin>>price[i];
+    }
+    for(int i=1;i<=n;i++)
+    {
+        cin>>pages[i];
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=0;j<=x;j++)
+        {
+            dp[i][j]=dp[i-1][j];
+            if(j-price[i]>=0)
+            dp[i][j]=max(dp[i][j],dp[i-1][j-price[i]]+pages[i]);
+        }
+    }
+    cout<<dp[n][x];
+}
